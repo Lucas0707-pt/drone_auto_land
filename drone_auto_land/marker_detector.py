609b59c46ada22f63f09f0fa9299d9f7ed30933b
Lucas <luislucas0707@gmail.com>
2024-05-21 18:55:55 +0100

    camera algorithm (incomplete)

--- drone_auto_land/marker_detector.py
+++ drone_auto_land/marker_detector.py
@@ -20,13 +20,22 @@ class MarkerDetector(Node):
 
         self.marker_id = 29
         self.embedded_marker_id = 33
+        self.marker_bits = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 1, 1, 0, 0, 1, 0, 1, 0],
+                                        [0, 1, 1, 0, 1, 1, 0, 1, 0],
+                                        [0, 0, 1, 1, 1, 0, 1, 1, 0],
+                                        [0, 1, 0, 1, 1, 1, 0, 0, 0],
+                                        [0, 1, 0, 1, 1, 1, 1, 0, 0],
+                                        [0, 1, 1, 0, 1, 1, 0, 0, 0],
+                                        [0, 0, 1, 1, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0]],dtype=np.uint8)
 
         self.marker_size = 0.291
         self.embedded_marker_size = 0.033
 
         # Load the camera matrix and distortion coefficients
-        camera_matrix_file = 'src/drone_auto_land/drone_auto_land/camera_parameters/camera_matrix.txt'
-        distortion_coefficients_file = 'src/drone_auto_land/drone_auto_land/camera_parameters/camera_distortion_coefficients.txt'
+        camera_matrix_file = 'camera_parameters/camera_matrix.txt'
+        distortion_coefficients_file = 'camera_parameters/camera_distortion_coefficients.txt'
         self.camera_matrix = np.loadtxt(camera_matrix_file, delimiter=',')
         self.distortion_coeffs = np.loadtxt(distortion_coefficients_file, delimiter=',')
 
@@ -39,19 +48,98 @@ class MarkerDetector(Node):
         pose.pose.position.z = z
         self.aruco_pose_camera_pub.publish(pose)
 
+    def image_binarization(self, img):
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        threshold_img = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+        return threshold_img
+    
+    def detect_corners(self, img, min_area, max_area):
+        contours, _ = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        
+        group_of_corners = []
+        for contour in contours:
+            area = cv.contourArea(contour)
+            precision = 0.04
+            epsilon = precision * cv.arcLength(contour, True)
+            approx = cv.approxPolyDP(contour, epsilon, True)
+
+            if (min_area < area < max_area and len(approx) == 4):
+                #approx = cv.convexHull(approx)
+                approx = approx.reshape((-1, 2))
+                group_of_corners.append(approx)
+                #self.get_logger().info(f"Area: {area}")
+
+        return group_of_corners
+
+        #self.get_logger().info(f"Number of corners detected: {len(group_of_corners[0])}")
+    
+    def verify_potential_marker(self, img, corners, markerBorderBits=1, minOtsuStdDev=5.0, perspectiveRemovePixelPerCell=4, perspectiveRemoveIgnoredMarginPerCell=0.13):
+        
+        dst_pts = np.array([[0, 0], [299, 0], [299, 299], [0, 299]], dtype=np.float32)
+
+        corners = corners.astype(np.float32)
+        M = cv.getPerspectiveTransform(corners, dst_pts)
+        warped = cv.warpPerspective(img, M, (300, 300))
+        
+        gray = cv.cvtColor(warped, cv.COLOR_BGR2GRAY)
+
+        #Apply otsu thresholding with gaussian filtering
+        gray = cv.GaussianBlur(gray, (3, 3), 0)
+        _, threshold_image = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+        # Perform marker detection
+        cell_size = threshold_image.shape[0] // 9
+        cell_margin = int(cell_size * perspectiveRemoveIgnoredMarginPerCell)
+        bits = np.zeros((9,9), dtype=np.uint8)
+
+        for y in range(9):
+            for x in range(9):
+                cell = threshold_image[y*cell_size:(y+1)*cell_size, x*cell_size:(x+1)*cell_size]
+                cell = cell[cell_margin:cell_size-cell_margin, cell_margin:cell_size-cell_margin]
+                if np.mean(cell) > 127:
+                    bits[y,x] = 1
+
+
+        bits_image = cv.cvtColor(bits.astype(np.uint8)*255, cv.COLOR_GRAY2BGR)
+        return bits_image
+        
+
+    
     def estimate_pose(self, corners, marker_size):
         ret = cv.aruco.estimatePoseSingleMarkers(corners, marker_size, self.camera_matrix, self.distortion_coeffs)
         _, tvec = ret[0][0,0,:], ret[1][0,0,:]
         return tvec
 
 
+
     # Callback function for the camera image
     def image_callback(self, msg):
-
+        # save the image
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        undistorted_img = cv.undistort(cv_image, self.camera_matrix, self.distortion_coeffs)
+        cv_image_bin = self.image_binarization(undistorted_img)
+        group_of_corners = self.detect_corners(cv_image_bin, 1000, 10000)
+
+        if len(group_of_corners) == 0:
+            return
+        
+        cv_image = self.verify_potential_marker(undistorted_img, group_of_corners[0])
+        #components = self.connected_components(cv_image_bin)
+        #cv_image = self.connected_components(cv_image_bin)
+        #contours, _ = cv.findContours(cv_image_bin, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        #draw the contours
+        #cv_image = cv.drawContours(cv_image, contours, -1, (0, 255, 0), 3)
+
+        """
         cv_image = cv.undistort(cv_image, self.camera_matrix, self.distortion_coeffs)
         gray = cv.cvtColor(cv_image, cv.COLOR_BGR2GRAY)
+        _, treshold_img = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+        contours, _ = cv.findContours(treshold_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        #Apply otsu thresholding with gaussian filtering
         gray = cv.GaussianBlur(gray, (3, 3), 0)
+        _, threshold_image = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
 
         # Perform marker detection
         aruco_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_7X7_1000)
@@ -92,7 +180,8 @@ class MarkerDetector(Node):
 
 
         # Publish the image with the detected markers
-        self.aruco_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8'))
+        """
+        self.aruco_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, encoding='rgb8'))
 
         
 def main(args=None):
