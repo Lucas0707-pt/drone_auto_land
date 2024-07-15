6fc821d52c242c76cdaf3e22628387c393dab866
Lucas <luislucas0707@gmail.com>
2024-07-15 15:39:45 +0100

    update on frame_converter.py

--- drone_auto_land/frame_converter.py
+++ drone_auto_land/frame_converter.py
@@ -35,14 +35,13 @@ class FrameConverter(Node):
 
         self.bridge = CvBridge()
 
+        self.vehicle_odometry_sub = self.create_subscription(
+            VehicleOdometry, '/fmu/out/vehicle_odometry', self.vehicle_odometry_callback, qos_profile)
+
         self.aruco_pose_camera_sub = self.create_subscription(
             PoseStamped, 'aruco_pose_camera', self.aruco_pose_camera_callback, 10)
         
-        self.vehicle_odometry_sub = self.create_subscription(
-            VehicleOdometry, '/fmu/out/vehicle_odometry', self.vehicle_odometry_callback, qos_profile)
-        
-        self.aruco_pose_local_pub = self.create_publisher(
-            PoseStamped, 'aruco_pose_local', 10)
+        self.aruco_pose_drone_pub = self.create_publisher(PoseStamped, 'aruco_pose_drone', 10)
         
         self.aruco_image_sub = self.create_subscription(Image, 'aruco_image_aux', self.aruco_image_callback, 10)
 
@@ -55,21 +54,18 @@ class FrameConverter(Node):
             'z': None
         }
 
-        self.aruco_pose_local = {
+        self.vehicle_odometry = {
+            'timestamp': None,
             'x': None,
             'y': None,
             'z': None
         }
 
-        self.vehicle_odometry = {
+        self.velocity_setpoint = {
             'timestamp': None,
-            'x': None,
-            'y': None,
-            'z': None,
-            'qw': None,
-            'qx': None,
-            'qy': None,
-            'qz': None
+            'vx': None,
+            'vy': None,
+            'vz': None
         }
 
         self.tvec_C_D = np.array([0.05, 0, 0.04])
@@ -79,67 +75,42 @@ class FrameConverter(Node):
         self.tvec_D_L = None
         self.rvec_D_L = None
 
-
-        self.acceptable_timestamp_diff = 3 * 1e5
-
-    def draw_frames(self, cv_image):
-        # Define the size of the frame
-        frame_size = 50
-
-        # Draw the camera frame (in red)
-        cv.arrowedLine(cv_image, (50, 50), (50 + frame_size, 50), (0, 0, 255), 2)
-        cv.arrowedLine(cv_image, (50, 50), (50, 50 - frame_size), (0, 0, 255), 2)
-
-        # Draw the drone frame (in green)
-        cv.arrowedLine(cv_image, (150, 50), (150 + frame_size, 50), (0, 255, 0), 2)
-        cv.arrowedLine(cv_image, (150, 50), (150, 50 - frame_size), (0, 255, 0), 2)
-
-        # Draw the local frame (in blue)
-        cv.arrowedLine(cv_image, (250, 50), (250 + frame_size, 50), (255, 0, 0), 2)
-        cv.arrowedLine(cv_image, (250, 50), (250, 50 - frame_size), (255, 0, 0), 2)
-
-        return cv_image
-
     def aruco_image_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         if (self.aruco_pose_camera['x'] is not None and self.aruco_pose_local['x'] is not None and self.vehicle_odometry['x'] is not None):
             aruco_pose_camera_text = f"ArUco Pose Camera: x={self.aruco_pose_camera['x']:.2f}, y={self.aruco_pose_camera['y']:.2f}, z={self.aruco_pose_camera['z']:.2f}"
             aruco_pose_local_text = f"ArUco Pose Local: x={self.aruco_pose_local['x']:.2f}, y={self.aruco_pose_local['y']:.2f}, z={self.aruco_pose_local['z']:.2f}"
-            vehicle_odometry_text = f"Vehicle Odometry: x={self.vehicle_odometry['x']:.2f}, y={self.vehicle_odometry['y']:.2f}, z={self.vehicle_odometry['z']:.2f}"
-
+            vehicle_odometry_text = f"Drone Position: x={self.vehicle_odometry['x']:.2f}, y={self.vehicle_odometry['y']:.2f}, z={self.vehicle_odometry['z']:.2f}"
             cv.putText(cv_image, aruco_pose_camera_text, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             cv.putText(cv_image, aruco_pose_local_text, (10, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             cv.putText(cv_image, vehicle_odometry_text, (10, 70), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            if (self.velocity_setpoint['vx'] is not None):
+                velocity_setpoint_text = f"Velocity Setpoint: vx={self.velocity_setpoint['vx']:.2f}, vy={self.velocity_setpoint['vy']:.2f}, vz={self.velocity_setpoint['vz']:.2f}"
+                cv.putText(cv_image, velocity_setpoint_text, (10, 90), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-        #cv_image = self.draw_frames(cv_image)
+        cv.putText(cv_image, aruco_pose_camera_text, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv.putText(cv_image, aruco_pose_local_text, (10, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv.putText(cv_image, vehicle_odometry_text, (10, 70), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
 
         if self.record:
             self.out.write(cv_image)
 
         self.aruco_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8'))
 
+    # Publish the pose of the detected ArUco marker
+    def publish_aruco_pose_drone(self, x, y, z):
+        pose = PoseStamped()
+        pose.header.stamp = self.get_clock().now().to_msg()
+        pose.pose.position.x = x
+        pose.pose.position.y = y
+        pose.pose.position.z = z
+        self.aruco_pose_drone_pub.publish(pose)
 
     def frame_conversion(self):
-        if (self.vehicle_odometry['timestamp'] is not None) and (self.aruco_pose_camera['timestamp'] is not None):
-            timestamp_diff = self.vehicle_odometry['timestamp'] - self.aruco_pose_camera['timestamp']
-            #self.get_logger().info('Timestamp difference: {}'.format(timestamp_diff))
-            if abs(timestamp_diff) < self.acceptable_timestamp_diff:
-                aruco_pose_message = PoseStamped()
-                aruco_pose_drone = self.convert_cam2drone()
-                self.tvec_D_L = np.array([self.vehicle_odometry['x'], self.vehicle_odometry['y'], self.vehicle_odometry['z']])
-                self.rvec_D_L = self.convert_quat2rot()
-                self.convert_drone2local(aruco_pose_drone)
-                aruco_pose_message.pose.position.x = self.aruco_pose_local['x']
-                aruco_pose_message.pose.position.y = self.aruco_pose_local['y']
-                aruco_pose_message.pose.position.z = self.aruco_pose_local['z']
-                self.aruco_pose_local_pub.publish(aruco_pose_message)
+        aruco_pose_drone = self.convert_cam2drone()
+        self.publish_aruco_pose_drone(aruco_pose_drone[0], aruco_pose_drone[1], aruco_pose_drone[2])
 
-
-    def convert_quat2rot(self):
-        return np.array([[1 - 2 * self.vehicle_odometry['qy']**2 - 2 * self.vehicle_odometry['qz']**2, 2 * self.vehicle_odometry['qx'] * self.vehicle_odometry['qy'] - 2 * self.vehicle_odometry['qz'] * self.vehicle_odometry['qw'], 2 * self.vehicle_odometry['qx'] * self.vehicle_odometry['qz'] + 2 * self.vehicle_odometry['qy'] * self.vehicle_odometry['qw']],
-                        [2 * self.vehicle_odometry['qx'] * self.vehicle_odometry['qy'] + 2 * self.vehicle_odometry['qz'] * self.vehicle_odometry['qw'], 1 - 2 * self.vehicle_odometry['qx']**2 - 2 * self.vehicle_odometry['qz']**2, 2 * self.vehicle_odometry['qy'] * self.vehicle_odometry['qz'] - 2 * self.vehicle_odometry['qx'] * self.vehicle_odometry['qw']],
-                        [2 * self.vehicle_odometry['qx'] * self.vehicle_odometry['qz'] - 2 * self.vehicle_odometry['qy'] * self.vehicle_odometry['qw'], 2 * self.vehicle_odometry['qy'] * self.vehicle_odometry['qz'] + 2 * self.vehicle_odometry['qx'] * self.vehicle_odometry['qw'], 1 - 2 * self.vehicle_odometry['qx']**2 - 2 * self.vehicle_odometry['qy']**2]])
-    
     def get_transform_matrix(self, tvec, rvec):
         T = np.eye(4)
         T[:3, :3] = rvec
@@ -152,14 +123,13 @@ class FrameConverter(Node):
         aruco_pose_camera = np.array([self.aruco_pose_camera['x'], self.aruco_pose_camera['y'], self.aruco_pose_camera['z'], 1])
         aruco_pose_drone = np.dot(T_C_D, aruco_pose_camera)
         return aruco_pose_drone
-        
 
-    def convert_drone2local(self, aruco_pose_drone):
-        T_D_L = self.get_transform_matrix(self.tvec_D_L, self.rvec_D_L)
-        aruco_pose_local = np.dot(T_D_L, aruco_pose_drone)
-        self.aruco_pose_local['x'] = aruco_pose_local[0]
-        self.aruco_pose_local['y'] = aruco_pose_local[1]
-        self.aruco_pose_local['z'] = aruco_pose_local[2]
+    def aruco_pose_camera_callback(self, aruco_pose_camera):
+        self.aruco_pose_camera['timestamp'] = aruco_pose_camera.header.stamp.sec * 1e6 + aruco_pose_camera.header.stamp.nanosec * 1e-3
+        self.aruco_pose_camera['x'] = aruco_pose_camera.pose.position.x
+        self.aruco_pose_camera['y'] = aruco_pose_camera.pose.position.y
+        self.aruco_pose_camera['z'] = aruco_pose_camera.pose.position.z
+        self.frame_conversion()
 
     def vehicle_odometry_callback(self, vehicle_odometry):
         self.vehicle_odometry['timestamp'] = vehicle_odometry.timestamp
@@ -171,12 +141,11 @@ class FrameConverter(Node):
         self.vehicle_odometry['qy'] = vehicle_odometry.q[2]
         self.vehicle_odometry['qz'] = vehicle_odometry.q[3]
 
-    def aruco_pose_camera_callback(self, aruco_pose_camera):
-        self.aruco_pose_camera['timestamp'] = aruco_pose_camera.header.stamp.sec * 1e6 + aruco_pose_camera.header.stamp.nanosec * 1e-3
-        self.aruco_pose_camera['x'] = aruco_pose_camera.pose.position.x
-        self.aruco_pose_camera['y'] = aruco_pose_camera.pose.position.y
-        self.aruco_pose_camera['z'] = aruco_pose_camera.pose.position.z
-        self.frame_conversion()
+    def trajectory_setpoint_callback(self, msg):
+        self.velocity_setpoint['timestamp'] = msg.timestamp
+        self.velocity_setpoint['vx'] = msg.velocity[0]
+        self.velocity_setpoint['vy'] = msg.velocity[1]
+        self.velocity_setpoint['vz'] = msg.velocity[2]
         
 def main(args=None):
     rclpy.init(args=args)
--- drone_auto_land/marker_detector.py
+++ drone_auto_land/marker_detector.py
@@ -9,36 +9,20 @@ from geometry_msgs.msg import PoseStamped
 class MarkerDetector(Node):
     def __init__(self):
         super().__init__('marker_detector')
-        
-        #create publishers and subscribers
-        self.camera_image_sub = self.create_subscription(Image, '/camera', self.image_callback, 10)
+
+        # Create publishers and subscribers
+        self.camera_image_sub = self.create_subscription(Image, 'camera', self.image_callback, 10)
         self.aruco_image_pub = self.create_publisher(Image, 'aruco_image_aux', 10)
         self.aruco_pose_camera_pub = self.create_publisher(PoseStamped, 'aruco_pose_camera', 10)
 
         self.bridge = CvBridge()
 
-        self.marker_id = 29
-        self.embedded_marker_id = 33
-        self.marker_bits = np.array([[1, 1, 0, 0, 1, 0, 1],
-                                        [1, 1, 0, 1, 1, 0, 1],
-                                        [0, 1, 1, 1, 0, 1, 1],
-                                        [1, 0, 1, 1, 1, 0, 0],
-                                        [1, 0, 1, 1, 1, 1, 0],
-                                        [1, 1, 0, 1, 1, 0, 0],
-                                        [0, 1, 1, 0, 0, 0, 0]],dtype=np.uint8)
-        
-        self.embedded_marker_bits = np.array([[0, 0, 1, 1, 1, 0, 1],
-                                              [0, 0, 1, 0, 0, 0, 1],
-                                              [1, 0, 0, 0, 1, 0, 0],
-                                              [1, 0, 0, 1, 1, 1, 0],
-                                              [1, 1, 1, 0, 1, 0, 0],
-                                              [0, 0, 1, 0, 0, 1, 1],
-                                              [1, 0, 1, 1, 1, 0, 1]], dtype=np.uint8)
+        self.marker_id = 118
+        self.embedded_marker_id = 345
+        self.marker_size = 0.20
+        self.embedded_marker_size = 0.029
 
-        self.marker_size = 0.291
-        self.embedded_marker_size = 0.033
-
-        # Load the camera matrix and distortion coefficients
+        # Load the camera matrix and distortion ro
         camera_matrix_file = 'src/drone_auto_land/drone_auto_land/camera_parameters/camera_matrix.txt'
         distortion_coefficients_file = 'src/drone_auto_land/drone_auto_land/camera_parameters/camera_distortion_coefficients.txt'
         self.camera_matrix = np.loadtxt(camera_matrix_file, delimiter=',')
@@ -53,214 +37,89 @@ class MarkerDetector(Node):
         pose.pose.position.z = z
         self.aruco_pose_camera_pub.publish(pose)
 
-    def image_binarization(self, img):
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        _, threshold_img = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-
-        return threshold_img
-    
-    def angle_vector(self, v1, v2):
-        cosine_angle = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-        return np.arccos(cosine_angle)
-    
-    def detect_corners(self, img, img_aux, min_area, max_area, min_angle, max_angle):
-        blurred = cv.GaussianBlur(img, (3, 3), 0)
-        lower_threshold = np.median(blurred)
-        upper_threshold = lower_threshold * 1.5
-        edges = cv.Canny(blurred, lower_threshold, upper_threshold)
-
-        contours, _ = cv.findContours(edges.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-        potential_marker_corners = []
-        for contour in contours:
-            area = cv.contourArea(contour)
-            precision = 0.04
-            epsilon = precision * cv.arcLength(contour, True)
-            approx = cv.approxPolyDP(contour, epsilon, True)
-            if (min_area < area < max_area and len(approx) == 4):
-                approx = approx.reshape((-1, 2)).astype(np.float32)
-                angles = []
-                for i in range(4):
-                    v1 = approx[(i+1)%4] - approx[i]
-                    v2 = approx[(i+2)%4] - approx[(i+1)%4]
-                    angles.append(self.angle_vector(v1, v2))
-                
-                if (all(angle > min_angle and angle < max_angle for angle in angles) or True):
-                    potential_marker_corners.append(approx)
-                    cv.drawContours(img_aux, [contour], -1, (0, 255, 0), 3)
-                    cv.putText(img_aux, f"{area:.2f}", tuple(map(int, approx[0])), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        return potential_marker_corners
-    
-    def get_bits(self, img, corners, perspectiveRemoveIgnoredMarginPerCell=0.13):
-        dst_pts = np.array([[0, 0], [299, 0], [299, 299], [0, 299]], dtype=np.float32)
-
-        corners = corners.astype(np.float32)
-        M = cv.getPerspectiveTransform(corners, dst_pts)
-        warped = cv.warpPerspective(img, M, (300, 300))
+    def invert_pose(self, rvec, tvec):
+        # Convert the rotation vector to a rotation matrix
+        R_camera_to_marker, _ = cv.Rodrigues(rvec)
         
-        gray = cv.cvtColor(warped, cv.COLOR_BGR2GRAY)
+        ##transpose this matrix
+        rvec_marker_to_camera = R_camera_to_marker.T
+        # Compute the translation vector of the camera in relation to the marker
+        tvec_marker_to_camera = -rvec_marker_to_camera @ tvec
 
-        #Apply otsu thresholding with gaussian filtering
-        gray = cv.GaussianBlur(gray, (3, 3), 0)
-        _, threshold_image = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+        return rvec_marker_to_camera, tvec_marker_to_camera
 
-        # Perform marker detection
-        cell_size = threshold_image.shape[0] // 9
-        cell_margin = int(cell_size * perspectiveRemoveIgnoredMarginPerCell)
-        bits = np.zeros((9,9), dtype=np.uint8)
-
-        for y in range(9):
-            for x in range(9):
-                cell = threshold_image[y*cell_size:(y+1)*cell_size, x*cell_size:(x+1)*cell_size]
-                cell = cell[cell_margin:cell_size-cell_margin, cell_margin:cell_size-cell_margin]
-                if np.mean(cell) > 127:
-                    bits[y,x] = 1
-        return bits
-    
-    def verify_potential_marker(self, bits, maxErroneousBitsInBorderRate, maxErroneousBitsInMarkerRate):
-
-        marker_id = None
-
-        total_bits = bits.shape[0] * bits.shape[1]
-
-        max_erroneous_bits_border = total_bits * maxErroneousBitsInBorderRate
-        max_erroneous_bits_marker = total_bits * maxErroneousBitsInMarkerRate
-
-        # Check if the border bits are correct
-        border_bits = np.concatenate((bits[0, :], bits[-1, :], bits[:, 0], bits[:, -1]))
-        erroneous_border_bits = np.sum(border_bits == 1)
-
-        if erroneous_border_bits > max_erroneous_bits_border:
-            return False, None
-        
-        #Check if the marker bits are correct
-        marker_bits = bits[1:8, 1:8]
-        erroneous_marker_bits = np.sum(marker_bits != self.marker_bits)
-        erroneous_embedded_marker_bits = np.sum(marker_bits != self.embedded_marker_bits)
-
-        #Check if the marker bits are correct in all rotations
-        for _ in range(4):
-            if erroneous_marker_bits <= max_erroneous_bits_marker:
-                marker_id = self.marker_id
-                break
-            if erroneous_embedded_marker_bits <= max_erroneous_bits_marker:
-                marker_id = self.embedded_marker_id
-                break
-
-            marker_bits = np.rot90(marker_bits)
-            erroneous_marker_bits = np.sum(marker_bits != self.marker_bits)
-            erroneous_embedded_marker_bits = np.sum(marker_bits != self.embedded_marker_bits)
-
-
-        return marker_id is not None, marker_id
-        
-
-        
-
-    
     def estimate_pose(self, corners, marker_size):
         ret = cv.aruco.estimatePoseSingleMarkers(corners, marker_size, self.camera_matrix, self.distortion_coeffs)
-        _, tvec = ret[0][0,0,:], ret[1][0,0,:]
-        return tvec
+        rvec, tvec = ret[0][0, 0, :], ret[1][0, 0, :]
 
-    def remove_inner_markers(self, marker_corners, marker_ids, minCenterDistance, minCornerDistance):
-        marker_centers = [np.mean(corners, axis=0) for corners in marker_corners]
-        marker_perimeters = [cv.arcLength(corners, True) for corners in marker_corners]
-        keep_indices = []
+        # Convert the rotation vector to a rotation matrix
+        rotation_matrix, _ = cv.Rodrigues(rvec)
 
-        for i in range(len(marker_corners)):
-            keep = True
+        # Print the rotation matrix
+        print("Rotation Matrix:\n", rotation_matrix)
+        
+        # Invert pose to get camera in relation to marker
+        rvec_marker, tvec_marker = self.invert_pose(rvec, tvec)
 
-            for j in range(len(marker_corners)):
-                if i != j: 
-                    # Calculate the distance between the centers of the markers
-                    center_distance = np.linalg.norm(marker_centers[i] - marker_centers[j])
-                    # Calculate the minimum distance between the corners of the markers
-                    corner_distances = [np.linalg.norm(corner_i - corner_j) for corner_i in marker_corners[i] for corner_j in marker_corners[j]]
-                    min_corner_distance = min(corner_distances)
-
-                    if center_distance < minCenterDistance and min_corner_distance < minCornerDistance:
-                        if marker_perimeters[i] < marker_perimeters[j]:
-                            keep = False
-                            break
-
-            if keep:
-                keep_indices.append(i)
-
-        marker_corners = [marker_corners[i] for i in keep_indices]
-
-        marker_ids = [marker_ids[i] for i in keep_indices]
-
-        return marker_corners, marker_ids
-    
-    def draw_markers(self, img, marker_corners, marker_ids):
-        for i in range(len(marker_corners)):
-            corners = marker_corners[i][0].astype(np.int32)
-            cv.polylines(img, [corners], True, (0, 255, 0), 2)
-
-            bottom_right_corner_id = np.argmax(np.sum(corners, axis=1))
-
-            text_position = tuple(corners[bottom_right_corner_id])
-            cv.putText(img, str(marker_ids[i][0]), text_position, cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        return img
-
+        # Print the inverted pose
+        #print("Inverted Rotation Vector (rvec_camera):", rvec_marker)
+        #print("Inverted Translation Vector (tvec_camera):", tvec_marker)
+        
+        return tvec_marker
 
     # Callback function for the camera image
     def image_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        undistorted_img = cv.undistort(cv_image, self.camera_matrix, self.distortion_coeffs)
-        undistorted_img_bin = self.image_binarization(undistorted_img)
-        undistorted_img_copy = undistorted_img.copy()
-        potential_marker_corners = self.detect_corners(undistorted_img_bin, undistorted_img_copy, 1000, 1000000, 1.40,1.75)
+        cv_image = cv.undistort(cv_image, self.camera_matrix, self.distortion_coeffs)
+        gray = cv.cvtColor(cv_image, cv.COLOR_BGR2GRAY)
+        gray = cv.GaussianBlur(gray, (3, 3), 0)
 
-        marker_ids = []
-        marker_corners = []
+        # Perform marker detection
+        aruco_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_ARUCO_ORIGINAL)
+        parameters = cv.aruco.DetectorParameters_create()
+        corners, ids, _ = cv.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
-        if len(potential_marker_corners) > 0:
+        # If at least one marker detected
+        if len(corners) > 0:
+            # Draw the detected markers
+            cv_image = cv.aruco.drawDetectedMarkers(cv_image, corners, ids)
 
-            for corners in potential_marker_corners:
-                potential_marker_bits = self.get_bits(undistorted_img, corners)
-                is_marker, marker_id = self.verify_potential_marker(potential_marker_bits, 0.05, 0.05)
-                if is_marker:
-                    marker_ids.append(marker_id)
-                    marker_corners.append(corners)
-            marker_corners, marker_ids = self.remove_inner_markers(marker_corners, marker_ids, 5, 20)
-            if (len(marker_ids) > 0):
-                marker_corners = tuple(np.array([corner], dtype=np.float32) for corner in marker_corners)
-                marker_ids = np.array(marker_ids, dtype=np.int32).reshape(-1, 1)
+            # Center of the marker
+            for i in range(len(corners)):
+                c = corners[i][0]
+                cx = int((c[0][0] + c[1][0] + c[2][0] + c[3][0]) / 4)
+                cy = int((c[0][1] + c[1][1] + c[2][1] + c[3][1]) / 4)
+                cv.circle(cv_image, (cx, cy), 5, (0, 0, 255), -1)
 
-                undistorted_img = self.draw_markers(undistorted_img, marker_corners, marker_ids)
+            tvec = None
+            #print(corners)
+            match len(ids):
+                case 1:
+                    tvec = self.estimate_pose(corners, (self.marker_size if ids[0][0] == self.marker_id else self.embedded_marker_size))
+                case 2:
+                    # Check which of the corners is the big marker and estimate its pose
+                    if ids[0][0] == self.marker_id:
+                        tvec = self.estimate_pose([corners[0]], self.marker_size)
+                    else:
+                        tvec = self.estimate_pose([corners[1]], self.marker_size)
+                case _:
+                    self.get_logger().info("More than 2 markers detected")
+            
+            if tvec is not None and tvec[2] > 3:
+                self.publish_aruco_pose(tvec[0], tvec[1], tvec[2])
 
-                tvec = None
-                match len(marker_ids):
-                    case 1:
-                        tvec = self.estimate_pose(marker_corners, (self.marker_size if marker_ids[0][0] == self.marker_id else self.embedded_marker_size))
-                    case 2:
-                        if marker_ids[0][0] == self.marker_id:
-                            tvec = self.estimate_pose([marker_corners[0]], self.marker_size)
-                        else:
-                            tvec = self.estimate_pose([marker_corners[1]], self.embedded_marker_size)
-                    case _:
-                        self.get_logger().info("More than 2 markers detected")
+            aruco_pose_camera_text = f"ArUco Pose Camera: x={tvec[0]:.2f}, y={tvec[1]:.2f}, z={tvec[2]:.2f}"
+            cv.putText(cv_image, aruco_pose_camera_text, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-                if tvec is not None:
-                    self.publish_aruco_pose(tvec[0], tvec[1], tvec[2])
+        # Publish the image with the detected markers
+        self.aruco_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8'))
 
-                    aruco_pose_camera_text = f"ArUco Pose Camera: x={tvec[0]:.2f}, y={tvec[1]:.2f}, z={tvec[2]:.2f}"
-                    cv.putText(undistorted_img, aruco_pose_camera_text, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        self.aruco_image_pub.publish(self.bridge.cv2_to_imgmsg(undistorted_img, encoding='bgr8'))
-
-        
 def main(args=None):
     rclpy.init(args=args)
     marker_detector = MarkerDetector()
     rclpy.spin(marker_detector)
     marker_detector.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
