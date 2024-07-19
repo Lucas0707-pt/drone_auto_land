53f39e9930c902b56211265b4bd742810fcf1716
Lucas <luislucas0707@gmail.com>
2024-07-19 13:12:20 +0100

    fixed_logs

--- logs/data_plot.py
+++ logs/data_plot.py
@@ -3,38 +3,38 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # Define the log directory 
-log_dir = '2024_4_30_18_57_36/'
+log_dir = '2024_7_19_10_24_4/'
 # Define the list of CSV filenames
-aruco_pose_camera_file = log_dir + "aruco_pose_camera.csv"
-aruco_pose_local_file = log_dir + "aruco_pose_local.csv"
+aruco_pose_drone_file = log_dir + "aruco_pose_drone.csv"
+aruco_pose_local_file = log_dir + "aruco_pose_drone.csv"
 vehicle_odometry_file = log_dir + "vehicle_odometry.csv"
 
 # Create a new figure with two subplots
 fig = plt.figure(figsize=(15, 8))
 
-# Subplot for Aruco pose camera data
+# Subplot for Aruco pose drone data
 ax1 = fig.add_subplot(121, projection='3d')
-ax1.set_title('Aruco Pose Camera')
+ax1.set_title('Aruco Pose Drone')
 ax1.set_xlabel('X')
 ax1.set_ylabel('Y')
 ax1.set_zlabel('Z')
 
-# Read Aruco pose camera CSV file into a DataFrame
-df_aruco_camera = pd.read_csv(aruco_pose_camera_file)
-x_aruco_camera = df_aruco_camera['x'].to_numpy()
-y_aruco_camera = df_aruco_camera['y'].to_numpy()
-z_aruco_camera = df_aruco_camera['z'].to_numpy()
-ax1.scatter(x_aruco_camera, y_aruco_camera, z_aruco_camera, color='r', s=10, label='Aruco Pose Camera')
+# Read Aruco pose drone CSV file into a DataFrame
+df_aruco_drone = pd.read_csv(aruco_pose_drone_file)
+x_aruco_drone = df_aruco_drone['position_x'].to_numpy()
+y_aruco_drone = df_aruco_drone['position_y'].to_numpy()
+z_aruco_drone = df_aruco_drone['position_z'].to_numpy()
+ax1.scatter(x_aruco_drone, y_aruco_drone, z_aruco_drone, color='r', s=10, label='Aruco Pose Drone')
 
 # Connect the points with lines in subplot 1
-ax1.plot(x_aruco_camera, y_aruco_camera, z_aruco_camera, color='r', linewidth=0.5, label='_nolegend_')
+ax1.plot(x_aruco_drone, y_aruco_drone, z_aruco_drone, color='r', linewidth=0.5, label='_nolegend_')
 
 # Highlight first and last points in subplot 1
-ax1.scatter(x_aruco_camera[0], y_aruco_camera[0], z_aruco_camera[0], color='black', s=50, label='First Point Camera')
-ax1.scatter(x_aruco_camera[-1], y_aruco_camera[-1], z_aruco_camera[-1], color='orange', s=50, label='Last Point Camera')
+ax1.scatter(x_aruco_drone[0], y_aruco_drone[0], z_aruco_drone[0], color='black', s=50, label='First Point Drone')
+ax1.scatter(x_aruco_drone[-1], y_aruco_drone[-1], z_aruco_drone[-1], color='orange', s=50, label='Last Point Drone')
 
 # Set equal aspect ratio for subplot 1
-ax1.set_box_aspect([max(x_aruco_camera)-min(x_aruco_camera), max(y_aruco_camera)-min(y_aruco_camera), max(z_aruco_camera)-min(z_aruco_camera)])
+ax1.set_box_aspect([max(x_aruco_drone)-min(x_aruco_drone), max(y_aruco_drone)-min(y_aruco_drone), max(z_aruco_drone)-min(z_aruco_drone)])
 
 ax1.legend()
 
@@ -47,16 +47,16 @@ ax2.set_zlabel('Z')
 
 # Read Aruco pose local CSV file into a DataFrame
 df_aruco_local = pd.read_csv(aruco_pose_local_file)
-x_aruco_local = df_aruco_local['x'].to_numpy()
-y_aruco_local = df_aruco_local['y'].to_numpy()
-z_aruco_local = df_aruco_local['z'].to_numpy()
+x_aruco_local = df_aruco_local['position_x'].to_numpy()
+y_aruco_local = df_aruco_local['position_y'].to_numpy()
+z_aruco_local = df_aruco_local['position_z'].to_numpy()
 ax2.scatter(x_aruco_local, y_aruco_local, z_aruco_local, color='g', s=5, label='Aruco Pose Local')
 
 # Read vehicle odometry CSV file into a DataFrame
 df_vehicle_odometry = pd.read_csv(vehicle_odometry_file)
-x_vehicle_odometry = df_vehicle_odometry['x'].to_numpy()
-y_vehicle_odometry = df_vehicle_odometry['y'].to_numpy()
-z_vehicle_odometry = df_vehicle_odometry['z'].to_numpy()
+x_vehicle_odometry = df_vehicle_odometry['position_x'].to_numpy()
+y_vehicle_odometry = df_vehicle_odometry['position_y'].to_numpy()
+z_vehicle_odometry = df_vehicle_odometry['position_z'].to_numpy()
 ax2.scatter(x_vehicle_odometry, y_vehicle_odometry, z_vehicle_odometry, color='b', s=5, label='Vehicle Odometry')
 ax2.invert_zaxis()
 
